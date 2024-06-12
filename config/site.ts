be539f056cf4eedc683f690e80aaf9c7e43f8928
aakashearth9 <aakash@earth9.net>
2024-06-12 09:21:20 +0545

    updated sit config

--- config/site.ts
+++ config/site.ts
@@ -3,11 +3,7 @@ import { SiteConfig } from "@/types"
 export const siteConfig: SiteConfig = {
     name: "Osho Pedia Nepal",
     heroSectionTitle: "Discover the wisdom of OSHO for the self-transformation",
-    description:
-        `Welcome to Osho Pedia, your compass for self-transformation and inner exploration. 
-        Embark on a journey inspired and guided by Osho's profound wisdom. 
-        Delve into Osho's visionary perspective on life, explore his simple yet scientifically crafted meditation techniques, immerse yourself in his enlightening discourses, books, and articles. 
-        Discover Osho's timeless insights on contemporary topics, enriching our collective wisdom and fostering personal growth.`,
+    description: `Discover the wisdom of OSHO for the self-transformation`,
     url: "https://oshopedia.online",
     ogImage: "https://oshopedia.online/og.jpg",
     links: {
